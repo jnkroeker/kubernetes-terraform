6d2f0456c95f4fe21cfb4de037fb3d56a6f04b70
John Kroeker <jnkroeker@gmail.com>
2023-01-10 20:23:27 -0500

    adjust CoreDNS setup and add kafka CRD

--- scripts/dns/deploy-coredns.sh
+++ scripts/dns/deploy-coredns.sh
@@ -2,11 +2,11 @@
 
 deploy_core_dns () {
     kubectl apply -f https://storage.googleapis.com/kubernetes-the-hard-way/coredns-1.8.yaml 
-
-    kubectl get pods -l k8s-app=kube-dns -n kube-system
 }
 
 # Verification
+
+# kubectl get pods -l k8s-app=kube-dns -n kube-system
 
 # Create a busybox deployment
 
