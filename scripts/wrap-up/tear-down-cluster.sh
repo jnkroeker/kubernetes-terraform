db67087d18d368ff093f0c648455f114d64b7c67
John Kroeker <jnkroeker@gmail.com>
2023-04-08 14:42:19 -0400

    tear down external access resources

--- scripts/wrap-up/tear-down-cluster.sh
+++ scripts/wrap-up/tear-down-cluster.sh
@@ -4,13 +4,23 @@ tear_down() {
 
     gcloud compute forwarding-rules delete kubernetes-forwarding-rule
 
+    gcloud compute forwarding-rules delete khyme-forwarding-rule
+
     gcloud compute target-pools delete kubernetes-target-pool
 
+    gcloud compute target-pools delete khyme-resources
+
     gcloud compute http-health-checks delete kubernetes
+
+    gcloud compute http-health-checks delete basic-check
 
     gcloud compute firewall-rules delete kubernetes-allow-health-check
 
     gcloud compute firewall-rules delete kubernetes-the-hard-way-allow-nginx-service
+
+    gcloud compute firewall-rules delete khyme-api-external-access
+
+    gcloud compute addresses delete khyme-static-ip
 
     gcloud compute routes delete kubernetes-route-10-200-0-0-24
 
